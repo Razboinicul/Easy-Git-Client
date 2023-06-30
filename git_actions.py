bad70644dc1ad083fe4f359dc63878cdd90f83f1
R9_ <neamtrares@naver.com>
2023-06-30 22:43:03 +0300

    "Updated Repo"

---
+++ git_actions.py
@@ -0,0 +1,16 @@
+import subprocess
+from os import chdir
+
+def clone_repo(git_path, path=None):
+    if path != None: subprocess.run(["git", "clone", git_path, path])
+    else: subprocess.run(["git", "clone", git_path])
+
+def commit(message="Updated Repo"):
+    subprocess.run(["git", "add", "*"])
+    subprocess.run(["git", "commit", "-a", "-m", f'"{message}"'])
+
+clone_repo("https://github.com/Razboinicul/TestRepo", "TestRepo")
+chdir("TestRepo")
+file = open("test", "w").close()
+chdir("..")
+commit()
